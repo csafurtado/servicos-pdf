ccc2d7a9888d33936b6dabfd97b1ecf0e0647b2a
Cristian <csafurtado.dev@gmail.com>
2024-02-29 16:42:45 -0300

    Implementa funcionalidade de juntar PDFs

--- GUIInterface.py
+++ GUIInterface.py
@@ -6,15 +6,14 @@ from tkinter import filedialog, messagebox
 
 from manipulators import PDFManipulator, ImageManipulator
 
+## Funções para a janela
+### Função para abrir janela de escolha de arquivo PDF
+def abrir_arquivo(label_item):
+    dir_filename = filedialog.askopenfilename(initialdir=".", title="Escolha o PDF", filetypes=(("PDF files","*.pdf"),("Image Files",["*.jpg","*.jpeg","*.png"]),("All files", "*.*")))
+    label_item["text"] = dir_filename if len(dir_filename) else "Nenhum arquivo escolhido"
+
 # Função da janela principal do programa
 def abre_janela_principal():
-    ## Funções para a janela
-    ### Função para abrir janela de escolha de arquivo PDF
-    def abrir_arquivo():
-        dir_filename =  filedialog.askopenfilename(initialdir=".", title="Escolha o PDF", filetypes=(("PDF files","*.pdf"),("Image Files",["*.jpg","*.jpeg","*.png"]),("All files", "*.*")))
-        label_arq_escolhido["text"] = dir_filename if len(dir_filename) else "Nenhum arquivo escolhido"
-        filedialog.askopenfilenames()
-
     # Instancia a janela e a customiza
     j_principal = Tk()
     j_principal.title("Serviços de PDF ao seu dispor")
@@ -23,19 +22,20 @@ def abre_janela_principal():
     j_principal.configure(bg=cor_fundo_janela, takefocus=True, height=300, width=500)
     j_principal.eval('tk::PlaceWindow . center')
 
-    # Coloca os botões das funções na janela e os organiza posicionalmente
-    botao_escolher_arquivo = Button(j_principal, command=abrir_arquivo, text="Escolher arquivo", bg=cor_fundo_janela, fg="white")
-    botao_slice = Button(j_principal, command= lambda: abre_janela_slice(j_principal, os.path.abspath(label_arq_escolhido["text"])), text="Cortar PDF", height=2, width=14, cursor='dot')
-    botao_transformar_pdf_em_img = Button(j_principal, command= lambda: abrir_janela_transforma_img_em_pdf(os.path.abspath(label_arq_escolhido["text"])), text="JPG para PDF", height=2, width=14, cursor="dot")
-
-    botao_escolher_arquivo.place(relx=0.5, rely=0.25, anchor=CENTER)
-    botao_slice.place(relx=0.1, rely=0.7, anchor="w")
-    botao_transformar_pdf_em_img.place(relx=0.9, rely=0.7, anchor="e")
-
     # Define textos(labels) a serem mostrados e define sua organização
     label_arq_escolhido = Label(j_principal, text="Nenhum arquivo escolhido", font=('Helvetica 10 bold'), fg="white", bg=cor_fundo_janela)
     label_arq_escolhido.place(relx=.5, rely=.15, anchor=CENTER)
-    # label_gambiarra_invisivel =
+
+    # Coloca os botões das funções na janela e os organiza posicionalmente
+    botao_escolher_arquivo = Button(j_principal, command= lambda: abrir_arquivo(label_arq_escolhido), text="Escolher arquivo", bg=cor_fundo_janela, fg="white")
+    botao_slice = Button(j_principal, command= lambda: abre_janela_slice(j_principal, os.path.abspath(label_arq_escolhido["text"])), text="Cortar PDF", height=2, width=14, cursor='dot')
+    botao_transformar_pdf_em_img = Button(j_principal, command= lambda: abrir_janela_transforma_img_em_pdf(os.path.abspath(label_arq_escolhido["text"])), text="JPG para PDF", height=2, width=14, cursor="dot")
+    botao_juntar_pdfs = Button(j_principal, command= lambda: abrir_janela_juntar_pdfs(j_principal), text="Juntar PDFs", height=2, width=14, cursor="dot")
+    
+    botao_escolher_arquivo.place(relx=0.5, rely=0.25, anchor=CENTER)
+    botao_slice.place(relx=0.1, rely=0.7, anchor="w")
+    botao_transformar_pdf_em_img.place(relx=0.9, rely=0.7, anchor="e")
+    botao_juntar_pdfs.place(relx=0.5, rely=0.7, anchor=CENTER)
 
     j_principal.mainloop()
 
@@ -61,7 +61,7 @@ def abre_janela_slice(j_principal, fonte_arq_pdf):
         messagebox.showwarning("ATENCAO", "Escolha um arquivo PDF válido!")
         return
     
-    manipuladorPDF = PDFManipulator(fonte_arq_pdf)
+    manipuladorPDF = PDFManipulator()
 
     # Cria janela que tem como pai a janela principal e define suas propriedades
     janela_base = Toplevel(j_principal)     
@@ -78,7 +78,7 @@ def abre_janela_slice(j_principal, fonte_arq_pdf):
     campo_lim_superior = Entry(janela_base, width=8)
     campo_lim_inferior = Entry(janela_base, width=8)
 
-    label_qtd_paginas_pdf = Label(janela_base, text=f"Quantidade de páginas: {manipuladorPDF.qtd_paginas}", fg="white", bg=cor_fundo_janela, font=('Helvetica 10 bold'))
+    label_qtd_paginas_pdf = Label(janela_base, text=f"Quantidade de páginas: {manipuladorPDF.retorna_qtd_paginas_pdf(fonte_arq_pdf)}", fg="white", bg=cor_fundo_janela, font=('Helvetica 10 bold'))
     label_qtd_paginas_pdf.place(relx=0.1, rely=0.8)
 
     label_lim_inferior.place(relx=0.1, rely=0.4)
@@ -87,7 +87,7 @@ def abre_janela_slice(j_principal, fonte_arq_pdf):
     campo_lim_superior.place(relx=0.2, rely=0.4)
 
     # Botão de executar o corte
-    botao_executar_slice = Button(janela_base, background="#cf5f5f", text="Cortar", width=10, height=3, command= lambda: manipuladorPDF.faz_slice_paginas_pdf(campo_lim_inferior.get(), campo_lim_superior.get()))
+    botao_executar_slice = Button(janela_base, background="#cf5f5f", text="Cortar", width=10, height=3, command= lambda: manipuladorPDF.faz_slice_paginas_pdf(fonte_arq_pdf, campo_lim_inferior.get(), campo_lim_superior.get()))
     botao_executar_slice.place(relx=0.7, rely=0.35)
     # Executa duas funções quando se clica no 'X' desta janela de slice
     # janela_base.protocol('WM_DELETE_WINDOW', func= lambda: faz_bloqueio_janela(1, j_principal))       
@@ -105,3 +105,34 @@ def abrir_janela_transforma_img_em_pdf(fonte_img):
     
     manipuladorImagem = ImageManipulator(fonte_img)
     manipuladorImagem.transforma_img_em_pdf()
+
+def abrir_janela_juntar_pdfs(j_principal):
+    manipuladorPDF = PDFManipulator()
+
+    # Cria janela que tem como pai a janela principal e define suas propriedades
+    janela_base = Toplevel(j_principal)     
+    janela_base.title("Selecione os arquivos PDFs para juntar")
+    janela_base.geometry(f"{400}x{300}+{int((j_principal.winfo_screenwidth()/2)-(300/2))}+{int((j_principal.winfo_screenheight()/2)-(200/2))}")
+    janela_base.resizable(False, False)
+    cor_fundo_janela = "#383fb8"
+    janela_base.configure(bg=cor_fundo_janela, takefocus=True)
+    janela_base.grab_set()
+
+    # Define textos(labels) a serem mostrados e define sua organização
+
+    # Labels e caixas de entrada
+    label_pdf1 = Label(janela_base, text="PDF 1", fg="white", bg=cor_fundo_janela, font=('Helvetica 10 bold'))
+    label_pdf2 = Label(janela_base, text="PDF 2", fg="white", bg=cor_fundo_janela, font=('Helvetica 10 bold'))
+    botao_escolher_pdf1 = Button(janela_base, command=lambda: abrir_arquivo(label_pdf1), text="Escolher arquivo", bg=cor_fundo_janela, fg="white")
+    botao_escolher_pdf2 = Button(janela_base, command=lambda: abrir_arquivo(label_pdf2), text="Escolher arquivo", bg=cor_fundo_janela, fg="white")
+
+    label_pdf1.place(relx=0.5, rely=0.15, anchor=CENTER)
+    botao_escolher_pdf1.place(relx=0.5, rely=0.3, anchor=CENTER)
+    label_pdf2.place(relx=0.5, rely=0.5, anchor=CENTER)
+    botao_escolher_pdf2.place(relx=0.5, rely=0.65, anchor=CENTER)
+
+    # Botão de executar o corte
+    botao_juntar = Button(janela_base, background="#f3e56b", text="Juntar", width=15, height=1, command= lambda: manipuladorPDF.juntar_pdfs(label_pdf1['text'], label_pdf2['text']))
+    botao_juntar.place(relx=0.5, rely=0.85, anchor=CENTER)
+
+    janela_base.mainloop() 
--- manipulators.py
+++ manipulators.py
@@ -1,5 +1,6 @@
 import os
 from time import sleep
+import traceback
 from PyPDF2 import PdfReader, PdfWriter 
 from PIL import Image
 import img2pdf
@@ -8,31 +9,30 @@ from tkinter import messagebox
 
 
 class PDFManipulator:
-    def __init__(self, path):
-        self.path = path
-        self.pdf = PdfReader(path)
-        self.qtd_paginas = len(self.pdf.pages)
-
     # Funções para manipular o PDF escolhido
-        
+
     ## Corta de i até j das páginas de um pdf
-    def faz_slice_paginas_pdf(self, limite_inf, limite_sup):
+    def faz_slice_paginas_pdf(self, path_pdf, limite_inf, limite_sup):
         # Cria o leitor (que recebe o arquivo PDF) e o escritor de arquivo PDF (que irá gerar um novo PDF)            
-        nome_arquivo = os.path.basename(self.path).split(".pdf")[0]
+        nome_arquivo = os.path.basename(path_pdf).split(".pdf")[0]
 
         try:
+            arq_pdf = PdfReader(path_pdf)
+            qtd_paginas_arq_pdf = len(arq_pdf.pages)
+
             limite_inf = int(limite_inf) 
             limite_sup = int(limite_sup)
 
-            if limite_inf > limite_sup or limite_inf <= 0 or self.qtd_paginas < limite_sup or self.qtd_paginas < limite_inf or limite_inf is None or limite_sup is None:
-                messagebox.showwarning("ERRO","Verifique se os numeros colocados estão dentro da quantidade de páginas que o PDF possui!")
+            if limite_inf >= limite_sup or limite_inf <= 0 or qtd_paginas_arq_pdf < limite_sup or qtd_paginas_arq_pdf < limite_inf or limite_inf is None or limite_sup is None:
+                messagebox.showwarning("ERRO","Verifique se os números colocados estão dentro da quantidade de páginas que o PDF possui!")
                 return
             
             writer = PdfWriter()
 
+
             with open(f'{nome_arquivo}_{limite_inf}_a_{limite_sup}.pdf', 'wb') as outfile:
                 for i in range(limite_inf-1, limite_sup):
-                    writer.add_page(self.pdf.pages[i])
+                    writer.add_page(arq_pdf.pages[i])
                     writer.write(outfile)
             
             sleep(1)
@@ -43,6 +43,51 @@ class PDFManipulator:
             messagebox.showwarning("ERRO","Valor dos campos devem ser números e não podem estar vazios!")
             
             return
+        
+    ## Junta dois Pdfs
+    def juntar_pdfs(self, path_pdf1, path_pdf2):
+        try:
+            arq_pdf1 = PdfReader(path_pdf1)
+            arq_pdf2 = PdfReader(path_pdf2)
+
+            nome_arquivo_pdf1 = os.path.basename(path_pdf1).split(".pdf")[0]
+            nome_arquivo_pdf2 = os.path.basename(path_pdf2).split(".pdf")[0]
+
+            print(nome_arquivo_pdf1, nome_arquivo_pdf2)
+
+            qtd_paginas_pdf1 = self.retorna_qtd_paginas_pdf(path_pdf1)
+            qtd_paginas_pdf2 = self.retorna_qtd_paginas_pdf(path_pdf2)
+
+            writer = PdfWriter()
+
+            with open(f"{nome_arquivo_pdf1}_e_{nome_arquivo_pdf2}.pdf", 'wb') as outfile:
+                for i in range(0, qtd_paginas_pdf1+qtd_paginas_pdf2):
+                    if i < qtd_paginas_pdf1:
+                        writer.add_page(arq_pdf1.pages[i])
+                        writer.write(outfile)
+                    
+                    else:
+                        writer.add_page(arq_pdf2.pages[i-qtd_paginas_pdf1])
+                        writer.write(outfile)
+
+            sleep(1)
+
+            messagebox.showinfo("SUCESSO","PDF cortado com sucesso!")            
+
+        except:
+            messagebox.showwarning("ERRO","Erro encontrado ao juntar PDFs! Verifique o caminho dos PDFs escolhidos!")
+            traceback.print_exc()
+            return
+
+    def retorna_qtd_paginas_pdf(self, path_pdf):
+        try:
+            arq_pdf = PdfReader(path_pdf)
+
+            return len(arq_pdf.pages)
+
+        except:
+            messagebox.showwarning("ERRO","Erro encontrado ao verificar a qtd de páginas do PDF!")
+
 
 class ImageManipulator:
     def __init__(self, path):
